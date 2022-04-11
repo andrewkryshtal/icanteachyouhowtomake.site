b5784e8140fa8dbf258076a847836efa1d2e9f9e
Andrii Kryshtal <a.kryshtal@omo.systems>
2022-04-11 12:13:30 +0300

    added battery api

--- src/components/MainHeader/MainHeaderContainer.js
+++ src/components/MainHeader/MainHeaderContainer.js
@@ -1,7 +1,14 @@
+import { useEffect } from 'react';
 import { MainHeaderComponent } from './MainHeaderComponent';
 import { useTitle } from '../../hooks/useHeaderContext';
 
 export const MainHeaderContainer = () => {
+  useEffect(() => {
+    (async () => {
+      const battery = await navigator.getBattery()
+      console.log({ battery })
+    })()
+  }, [])
   const { state: headerTitle } = useTitle();
   return <MainHeaderComponent headerTitle={headerTitle} />
 }
